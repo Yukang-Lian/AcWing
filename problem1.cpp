29bb25caff329c0ba01bbfd708ce7ede93abf87e
Yukang Lian <Yukang.Lian2022@gmail.com>
2022-12-29 16:14:26 +0800

    format

--- problem1.cpp
+++ problem1.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 
-int main(void){
+int main(void) {
     int a, b;
-    std::cin>>a>>b;
-    std::cout<<a+b<<std::endl;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
     return 0;
 }
